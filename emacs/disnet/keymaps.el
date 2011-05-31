5a3af989b2edbc83acf8bd7301c5f533ec4da1ba
Tim Disney <tim.disney@gmail.com>
2011-05-30 23:22:26 -0700

    forcing a change

--- emacs/disnet/keymaps.el
+++
@@ -1,5 +0,0 @@
-; easier M-x
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-c\C-m" 'execute-extended-command)
-
-(global-set-key "\C-cbs" 'weblogger-start-entry)
--- emacs/disnet/misc.el
+++
@@ -1,5 +0,0 @@
-(defun create-tags (dir-name)
-  "Create tags file."
-  (interactive "Directory: ")
-  (eshell-command
-   (format "find %s -type f -name \"*.el\" | etags -" dir-name)))
